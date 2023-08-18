b4a309ac97ec3c36e002c337bc7434eec902e470
Tony Wu <tonywu6@protonmail.com>
2023-08-18 16:51:35 +0800

    chore(ci): update pipelines

--- patches/heu/docs/conf.py
+++ patches/heu/docs/conf.py
@@ -13,11 +13,6 @@
 
 # -- Project information -----------------------------------------------------
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("."))
-
 project = "HEU"
 copyright = "2023 Ant Group Co., Ltd"
 author = "HEU authors"
