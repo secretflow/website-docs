8e9dd392845a1f05a8cc4878318b3500e5f1840a
Tony Wu <tonywu6@protonmail.com>
2023-08-18 17:34:32 +0800

    fix(secretflow): update script

--- patches/secretflow/build_docs.sh
+++ patches/secretflow/build_docs.sh
@@ -2,5 +2,8 @@
 
 pip install -r requirements.txt -r docs/requirements.txt
 
+rm -rf docs/source
+SPHINX_APIDOC_OPTIONS=members,autosummary sphinx-apidoc -f -d 2 -t templates -o docs/source secretflow
+
 python -m sphinx -T -E -b mdx -D language=en docs docs/_build/mdx/en-US
 python -m sphinx -T -E -b mdx -D language=zh_CN docs docs/_build/mdx/zh-Hans
