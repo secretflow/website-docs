4514caa640ebc6ec2a50584b72d877ce658f3d41
Tony Wu <tonywu6@protonmail.com>
2023-08-18 18:22:04 +0800

    fix(spu): update script

--- patches/spu/build_docs.sh
+++ patches/spu/build_docs.sh
@@ -2,6 +2,8 @@
 
 pip install -r requirements.txt -r docs/requirements.txt
 
-python -c 'from spu import Runtime'
+pushd docs # this prevents Python from importing unbuilt SPU
 
-# python -m sphinx -T -E -b mdx -D language=en docs docs/_build/mdx/en-US
+python -m sphinx -T -E -b mdx -D language=en . ./_build/mdx/en-US
+
+popd
